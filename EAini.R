14d16316985c6946d108b6e2da336866e77ddd89
Almond, Russell <almond@acm.org>
2020-05-21 20:14:18 -0400

    First round of debugging.

--- EAini.R
+++ EAini.R
@@ -1,46 +1,20 @@
 ## Adjust this to your configuration directory.
 
-
-
 Proc4.config <- fromJSON("Proc4.json")
-apps <- Proc4.config$apps[appStem]
 
 dbhost <- "localhost"
 dbuser <- "" # "EAP"
 dbport <- "" # "27018"
 
-builduri <- function (host,username,password,port) {
-    security <- ""
-    if (nchar(username) > 0L) {
-        if (nchar(password) > 0L)
-            security <- paste(username,password,sep=":")
-        else
-            security <- username
-    }
-    if (nchar(port) > 0L)
-        host <- paste(host,port,sep=":")
-    else
-        host <- host
-    if (nchar(security) > 0L)
-        host <- paste(security,host,sep="@")
-    paste("mongodb:/",host,sep="/")
-}
+dburi <-Proc4::makeDBuri(dbuser, Proc4.config$pwds[[dbuser]],
+                         dbhost, dbport)
 
-dburi <-builduri(dbhost, dbuser,
-                 Proc4.config$pwds[[dbuser]],
-                 port)
+EAeng.local <- list(dburi=dburi,dbname="EARecords",admindbname="Proc4")
 
 
+config.dir <- "/home/ralmond/Projects/PP-EA"
 
-EAeng.local <- list(dburi=dburi,dbname="EARecords",P4dbname="Proc4")
-
-lognames <- sapply(appStem,
-                   function (app) sub("<app>",app,EAeng.config$logname))
-
-
-
-logfile <- file.path("/usr/local/share/Proc4/logs",lognames)
-
+logpath <- "/usr/local/share/Proc4/logs"
 
 NeticaLicenseKey <- ""
 
--- bin/RunEABN.R
+++ bin/RunEABN.R
@@ -1,5 +1,6 @@
-library(EABN)
 library(R.utils)
+library(EABN)
+library(PNetica)
 
 if (interactive()) {
     branch <- "PP-main"
@@ -9,3 +10,163 @@ if (interactive()) {
 
 system2("git", "pull")
 system2("git", c("checkout",branch))
+
+source("/usr/local/share/Proc4/EAini.R")
+
+EA.config <- fromJSON(file.path(config.dir,"config.json"),FALSE)
+
+appStem <- as.character(EA.config$appStem)
+
+apps <- Proc4.config$apps[appStem]
+if (length(apps)==0L || any (is.na(apps))) {
+  stop("Could not find apps for ",appStem)
+}
+
+sess <- NeticaSession(LicenseKey=NeticaLicenseKey)
+startSession(sess)
+
+if (isTRUE(EA.config$rebuildNets)) {
+
+  EA.tables <- EA.config$Tables
+  tid <- EA.tables$TableID
+  if (!is.null(tid) && nchar(tid)>0L) {
+    ## Read from Google sheet
+    templatURL <- paste("https://docs.google.com/spreadsheets/d",tid,
+                        "/gviz/tq?tqx=out:csv&sheet={%s}",
+                        sep="/")
+  } else {
+    ## Read from the tables directory
+    templateURL <- file.path(config.dir,"tables","%s.csv")
+  }
+  stattab <- read_csv(sprintf(templateURL,EA.tables$StatName),
+                      stringsAsFactors=FALSE,strip.white=TRUE)
+  netman <- read.csv(sprintf(templateURL,EA.tables$NetsName),
+                     stringsAsFactors=FALSE,strip.white=TRUE)
+  nodeman <- read.csv(sprintf(templateURL,EA.tables$NodeName),
+                      stringsAsFactors=FALSE,strip.white=TRUE)
+  omega <- read.csv(sprintf(templateURL,EA.tables$OmegaName),
+                    stringsAsFactors=FALSE,strip.white=TRUE)
+  QQ <- read.csv(sprintf(templateURL,EA.tables$QName),
+                 stringsAsFactors=FALSE,strip.white=TRUE)
+  ## Now build the models
+  Nethouse <- PNetica::BNWarehouse(manifest=netman,session=sess,key="Name")
+  Nodehouse <- PNetica::NNWarehouse(manifest=nodeman,
+                           key=c("Model","NodeName"),
+                           session=sess)
+
+  ## Build Competency Model
+  if (any(is.na(Omega$Node))) {
+    flog.error("Missing node name in rows: ",
+               which(is.na(Omega$Node))+1,capture=TRUE)
+  }
+  CM <- WarehouseSupply(Nethouse,EA.tables$CMName)
+  CM <- Omega2Pnet(Omega,CM,Nodehouse,override=TRUE)
+
+  ## Build Evidence Models
+  if (any(is.na(QQ$Model))) {
+    flog.error("Missing model name in rows: ",
+               which(is.na(QQ$Model))+1,capture=TRUE)
+  }
+  if (any(is.na(QQ$Node))) {
+    flog.error("Missing node name in rows: ",
+               which(is.na(QQ$Node))+1,capture=TRUE)
+  }
+  Qmat2Pnet(QQ,Nethouse,Nodehouse)
+  write.csv(netman,file.path(config.dir,"nets",
+                             EA.config$EAEngine$manifestFile))
+  for (name in netman$Name) {
+    if (nchar(name)>0L) {
+      net <- WarehouseSupply(Nethouse,name)
+      WriteNetworks(net,file.path(config.dir,"nets",PnetPathname(net)))
+    }
+  }
+  write.csv(stattab,file.path(config.dir,"nets",statFile))
+
+
+}
+
+##<<HERE>> Need to make the run in parallel.
+for (app in apps) {
+  sapp <- basename(app)
+
+  flog.appender(appender.file(file.path(logpath,
+                                   sub("<app>",sapp,EA.config$logname))))
+  flog.threshold(EA.config$loglevel)
+  listeners <- lapply(EA.config$listeners, buildListener,app,dburi)
+  names(listeners) <- sapply(listeners,listenerName)
+
+  EAeng.params <- c(EA.config$EAEngine,EAeng.local)
+  EAeng.params$histNodes <- as.character(EAeng.params$histNodes)
+  EAeng.params$listenerSet <-
+    ListenerSet(sender= sub("<app>",sapp,EA.config$sender),
+                dbname=EA.config$dbname, dburi=dburi,
+                listeners=listeners,
+                colname=EA.config$lscolname)
+  netman <- read.csv(file.path(config.dir,"nets",
+                               EA.config$EAEngine$manifestFile),
+                     stringsAsFactors=FALSE,strip.white=TRUE)
+  EAeng.params$warehouse <-
+    PNetica::BNWarehouse(manifest=netman,
+                         session=sess,key="Name",
+                         address=file.path(config.dir,"nets"))
+
+  eng <- do.call(ifelse(dburi=="",BNEngineNDB,BNEngineMongo),
+                 EAeng.params)
+  loadManifest(eng,netman)
+  stattab <- read.csv(file.path(config.dir,"nets",
+                                 EA.config$EAEngine$statFile),
+                       stringsAsFactors=FALSE,strip.white=TRUE)
+  configStats(eng,stattab)
+
+  if (!is.null(EA.config$listenerReset)) {
+    resetListeners(eng$listenerSet,as.character(EA.config$listenerReset),app)
+  }
+  if (dburi != "") {
+    if (EA.config$filter$doRemove) {
+      rquery <- buildJQuery(c(list(app=app,EA.config$filter$remove)))
+      eng$evdienceSets()$remove(rquery)
+    }
+    if (isTRUE(EA.config$SRreset)) {
+      eng$studentRecords()$clearAll(TRUE)   #Clear default, as we will set
+                                        #it back up in a moment.
+    }
+    if (isTRUE(EA.config$filter$doReprocess)) {
+      rquery <- buildJQuery(c(list(app=app,EA.config$filter$update)))
+      eng$evidenceSets()$update(rquery,'{"$set":{"processed":false}}')
+    }
+  }
+  setupDefaultSR(eng)
+  if (EA.config$limitNN=="ALL") {
+    eng$processN <- eng$evidenceSets()$count(buildJQuery(app=app,
+                                                         processed=FALSE))
+  } else {
+    eng$processN <- as.numeric(EA.config$limitNN)
+  }
+  flog.info("Begining EA for application %s.",basename(app))
+  if (is.finite(eng$processN)) {
+    flog.info("%d messages queued.",eng$processN)
+  } else {
+    flog.info("Running in server mode.")
+  }
+  mainLoop(eng)
+
+  if (!is.null(EA.config$statListener)) {
+    sl <- listeners[[EA.config$statListener]]
+    if (is.null(sl)) {
+      flog.error("Stat listener %s not found, skipping building stat file.",
+                 EA.config$statListener)
+    } else {
+      stat1 <- sl$messdb()$find(buildJQuery(app=app))
+      sdat <- data.frame(stat1[,c("app","uid","context","timestamp")],
+                         flatten(stat1$data))
+      sdat$app <- basename(sdat$app)
+      fname <- gsub("<app>",sapp,EA.config$statfile)
+      write.csv(stat1,fname)
+      file.copy(fname,
+                file.path("/www1/www/https_docs/PhysicsPlayground",
+                          basename(fname)),
+                overwrite=TRUE)
+    }
+  }
+}
+
